3dad57ecd6e349a613e1eb2236c97b91c8c3f783
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-06-23 11:12:26 +0800

    Complete fourth tutorial

---
+++ blog-pusher-api/app/Http/Controllers/BookController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+use App\Http\Resources\BookResource;
+
+class BookController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth:api')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return BookResource::collection(Book::with('ratings')->paginate(25));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create([
+            'user_id' => $request->user()->id,
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        return new BookResource($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        // check if currently authenticated user is the owner of the book
+        if ($request->user()->id !== $book->user_id) {
+            return response()->json(['error' => 'You can only edit your own books.'], 403);
+        }
+
+        $book->update($request->only(['title', 'description']));
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return response()->json(null, 204);
+    }
+}
