23d1cab8e6f5b4d063f85cdb721859f0c47e4af1
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-08-22 04:08:26 +0700

    Add new tutorial

---
+++ toptal-api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\factory::create();
+
+        $password = Hash::make('test123');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password
+        ]);
+
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password
+            ]);
+        }
+    }
+}
